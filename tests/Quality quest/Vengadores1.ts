f5b56294e8f6fe970e1b8443a3cbe718c5132e71
Andy <202111346@est.umss.edu>
2025-11-29 01:19:21 -0400

    Actualizo test

--- tests/Quality quest/Vengadores1.ts
+++ tests/Quality quest/Vengadores1.ts
@@ -2,7 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://front-segundo-sprint.vercel.app/');
-  await page.getByRole('link', { name: 'Agregar Disponibilidad' }).click();
-  await page.getByText('29').click();
-  await page.getByRole('button', { name: 'Atrás' }).click();
+  await page.getByRole('link', { name: 'Mis Trabajos (Vista-Cliente)' }).click();
+  await page.getByRole('button', { name: 'Ver Detalles' }).first().click();
+  await page.getByRole('button', { name: 'Calificar Proveedor' }).click();
+  await page.getByRole('button').filter({ hasText: /^$/ }).nth(4).click();
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Enviar' }).click();
 });
