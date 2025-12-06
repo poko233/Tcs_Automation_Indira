c5a9a7a0776aff50ab4b6a18fa423db887ee7321
Andy <202111346@est.umss.edu>
2025-12-06 01:39:03 -0400

    Actualizo tests

---
+++ tests/Quality quest/Vengadores-Yusara-PS-TC22.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://servineo-delta.vercel.app/Homepage');
+  await page.getByRole('button', { name: 'Iniciar Sesion' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('hector@gmail.com');
+  await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('12345Hc@');
+  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
+  await page.getByRole('link', { name: 'Mis Trabajos (Vista-Cliente)' }).click();
+  await page.getByRole('button', { name: 'Volver' }).click();
+});
